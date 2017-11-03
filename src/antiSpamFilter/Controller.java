3e0b4c93aa30c3daa71ad1291da42db357261d74
Bruno <Bruno@Bruno-PC>
2017-11-03 14:13:24 +0000

    Junção GUI e Controller
    Botão aleatório e Iniciar 
    
    Cores para classificar a configuração gerada

--- src/antiSpamFilter/Controller.java
+++ src/antiSpamFilter/Controller.java
@@ -3,181 +3,189 @@ package antiSpamFilter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Controller {
 
 	private String rulesPath;
 	private String hamPath;
 	private String spamPath;
-	private ArrayList <Rule> rules;
-	private ArrayList <Email> ham;
-	private ArrayList <Email> spam;
-	
+	private ArrayList<Rule> rules;
+	private ArrayList<Email> ham;
+	private ArrayList<Email> spam;
+
 	public Controller() {
-		this.rulesPath= "null";
 		this.rules = new ArrayList<>();
 		this.ham = new ArrayList<>();
 		this.spam = new ArrayList<>();
 	}
 
 	// Definição caminho do ficheiro rules.cf
-	
+
 	public void setRulesPath(String path) {
 		this.rulesPath = path;
 	}
-	
+
 	// Definição caminho do ficheiro ham.txt
-	
+
 	public void setHamPath(String path) {
 		this.hamPath = path;
 	}
-	
+
 	// Definição caminho do ficheiro spam.txt
-	
+
 	public void setSpamPath(String path) {
 		this.spamPath = path;
 	}
-	
+
 	// Leitura do ficheiro rules.cf, criação de Rules e adição ao vetor
+
 	public void readRules() {
 		try {
-		      FileReader ficheiro = new FileReader(this.rulesPath);
-		      BufferedReader leitor = new BufferedReader(ficheiro);
-		      String linha = leitor.readLine();
-		      while (linha != null) {
-		    	  Rule r = new Rule(linha);
-		    	  rules.add(r);
-		    	  System.out.println(linha);
-		    	  linha = leitor.readLine();
-		      }
-		     	      
-		      ficheiro.close();
-		    } catch (IOException e) {
-		    	  System.err.printf("Erro na abertura do arquivo: %s.\n",
-		          e.getMessage());
-		    }
+			FileReader ficheiro = new FileReader(this.rulesPath);
+			BufferedReader leitor = new BufferedReader(ficheiro);
+			String linha = leitor.readLine();
+			while (linha != null) {
+				Rule r = new Rule(linha);
+				rules.add(r);
+				linha = leitor.readLine();
+			}
+			System.out.println("Regras lidas");
+			ficheiro.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+		}
 	}
-	
+
 	// Leitura do ficheiro ham.txt, criação de Emails Ham e adição ao vetor
-	
+
 	public void readHam() {
 		try {
-		      FileReader ficheiro = new FileReader(this.hamPath);
-		      BufferedReader leitor = new BufferedReader(ficheiro);
-		      String linha = leitor.readLine();
-		      while (linha != null) {
-		    	  System.out.println(linha);
-		    	  String[] tokens = linha.split("\\s+");
-		    	  Email e = new Email(tokens[0]);
-		    	  for(int i = 1 ; i<tokens.length;i++) {
-		    		  e.addRule(tokens[i]);
-		    		  System.out.println(tokens[i]);
-		    	  }
-		    	  ham.add(e);
-		    	  linha = leitor.readLine();
-		      }
-		      
-		      ficheiro.close();
-		    } catch (IOException e) {
-		    	  System.err.printf("Erro na abertura do arquivo: %s.\n",
-		          e.getMessage());
-		    }
+			FileReader ficheiro = new FileReader(this.hamPath);
+			BufferedReader leitor = new BufferedReader(ficheiro);
+			String linha = leitor.readLine();
+			while (linha != null) {
+				String[] tokens = linha.split("\\s+");
+				Email e = new Email(tokens[0]);
+				for (int i = 1; i < tokens.length; i++) {
+					e.addRule(tokens[i]);
+				}
+				ham.add(e);
+				linha = leitor.readLine();
+			}
+			System.out.println("Ham lido");
+			ficheiro.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+		}
 	}
-	
+
 	// Leitura do ficheiro spam.txt, criação de Emails Spam e adição ao vetor
-	
+
 	public void readSpam() {
 		try {
-		      FileReader ficheiro = new FileReader(this.spamPath);
-		      BufferedReader leitor = new BufferedReader(ficheiro);
-		      String linha = leitor.readLine();
-		      while (linha != null) {
-		    	  System.out.println(linha);
-		    	  String[] tokens = linha.split("\\s+");
-		    	  Email e = new Email(tokens[0]);
-		    	  for(int i = 1 ; i<tokens.length;i++) {
-		    		  e.addRule(tokens[i]);
-		    	  }
-		    	  spam.add(e);
-		    	  linha = leitor.readLine();
-		      }
-		      
-		      ficheiro.close();
-		    } catch (IOException e) {
-		    	  System.err.printf("Erro na abertura do arquivo: %s.\n",
-		          e.getMessage());
-		    }
+			FileReader ficheiro = new FileReader(this.spamPath);
+			BufferedReader leitor = new BufferedReader(ficheiro);
+			String linha = leitor.readLine();
+			while (linha != null) {
+				String[] tokens = linha.split("\\s+");
+				Email e = new Email(tokens[0]);
+				for (int i = 1; i < tokens.length; i++) {
+					e.addRule(tokens[i]);
+				}
+				spam.add(e);
+				linha = leitor.readLine();
+			}
+			System.out.println("Spam lido");
+			ficheiro.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+		}
 	}
-	
+
+	// Preenchimento da tabela das regras
+
+	public String[][] preencherTabela() {
+		if (rules.isEmpty()) {
+			String[][] dados = new String[][] {};
+			return dados;
+		} else {
+			DecimalFormat df = new DecimalFormat("#.###");
+			df.setRoundingMode(RoundingMode.CEILING);
+			
+			String[][] dados = new String[rules.size()][2];
+			for (int i = 0; i < rules.size(); i++) {
+				dados[i][0] = rules.get(i).getName();
+				dados[i][1] = String.valueOf(df.format(rules.get(i).getPeso()));
+			}
+			return dados;
+		}
+
+	}
+
 	// Consulta do peso de uma determinada regra
-	
+
 	public double getPeso(String rule) {
-		for(Rule r : rules) {
-			if(r.getName().equals(rule))
+		for (Rule r : rules) {
+			if (r.getName().equals(rule))
 				return r.getPeso();
 		}
 		return 0;
 	}
-	
-	// Cálculo de Falsos Positivos recorrendo aos emails Ham considerados SPAM ( somatório > 5)
-	
+
+	// Cálculo de Falsos Positivos recorrendo aos emails Ham considerados SPAM (
+	// somatório > 5)
+
 	public int calcularFP() {
 		int falsosPositivos = 0;
-		
-		for(Email e : ham) {
+
+		for (Email e : ham) {
 			int somatório = 0;
-			
-			for(String r :e.getRules()) {
+
+			for (String r : e.getRules()) {
 				somatório += getPeso(r);
 			}
-			if(somatório>5)
+			if (somatório > 5)
 				falsosPositivos++;
 		}
 		return falsosPositivos;
-		
+
 	}
-		
-	// Cálculo de Falsos Negativos recorrendo aos emails Spam considerados HAM ( somatório < 5)
-	
+
+	// Cálculo de Falsos Negativos recorrendo aos emails Spam considerados HAM (
+	// somatório < 5)
+
 	public int calcularFN() {
 		int falsosNegativos = 0;
-		
-		for(Email e : spam) {
+
+		for (Email e : spam) {
 			int somatório = 0;
-			
-			for(String r :e.getRules()) {
+
+			for (String r : e.getRules()) {
 				somatório += getPeso(r);
 			}
-			if(somatório<=5)
+			if (somatório <= 5)
 				falsosNegativos++;
 		}
 		return falsosNegativos;
-		
 	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	public void pesosAleatorios() {
+		for(Rule r : rules) {
+			r.setPeso(ThreadLocalRandom.current().nextDouble(-5,5));
+		}
+	}
+	
+	public boolean ficheirosDef() {
+		if(rulesPath==null || hamPath==null || spamPath==null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -20,53 +20,58 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JLabel;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FilenameFilter;
+
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
 
 @SuppressWarnings("serial")
 public class GUI {
-
+	private Controller controller = new Controller();
 	private JFrame janelaPrincipal;
 	private JPanel painel;
-	static final double fator=0.5;
-	private static Font f =new Font("Century Gothic", Font.PLAIN,18);
-	private static Font f2 =new Font("Century Gothic", Font.PLAIN,16);
+	static final double fator = 0.5;
+	private static Font f = new Font("Century Gothic", Font.PLAIN, 18);
+	private static Font f2 = new Font("Century Gothic", Font.PLAIN, 16);
+	private static Font f3 = new Font("Century Gothic", Font.BOLD, 20);
 	private JTextField info;
 	private JTextField fp;
 	private JTextField fn;
 	private JTable table;
 	private JScrollPane scroll;
 	final JFileChooser fc = new JFileChooser();
-	
+	private String[][] dados;
+	private DefaultTableModel model;
+	private String[] colunas;
+	private int botao = 0;
+
 	public static void main(String[] args) {
 		GUI janela = new GUI();
 	}
 
-// FRAME
-	
+	// FRAME
+
 	public GUI() {
-//configuração da janela
+		// configuração da janela
 		janelaPrincipal = new JFrame("AntiSpammers");
 		janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		janelaPrincipal.setBounds(0,0,(int) (screenSize.height*fator*1.5625),(int) (screenSize.height*fator));
-		System.out.println(screenSize.width*fator +"X"+screenSize.height*fator);
+		janelaPrincipal.setBounds(0, 0, (int) (screenSize.height * fator * 1.5625), (int) (screenSize.height * fator));
+		System.out.println(screenSize.width * fator + "X" + screenSize.height * fator);
 		painel = new JPanel();
 		painel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		janelaPrincipal.setContentPane(painel);
 		painel.setLayout(new MigLayout("", "[grow][grow][grow][][grow]", "[100px][100px][][][250px][100px][][]"));
 		janelaPrincipal.setResizable(false);
-		janelaPrincipal.setLocationRelativeTo(null);	
-		
-//MENU
-		
+		janelaPrincipal.setLocationRelativeTo(null);
+
+		// MENU
+
 		JMenuBar menuBar = new JMenuBar();
 		JMenu ficheiros = new JMenu("Ficheiros");
 		JMenu guardar = new JMenu("Guardar configuração");
@@ -75,178 +80,259 @@ public class GUI {
 		ficheiros.setFont(f2);
 		guardar.setFont(f2);
 		historico.setFont(f2);
-		
+
 		menuBar.add(ficheiros);
 		menuBar.add(guardar);
 		menuBar.add(historico);
-		
+
 		JMenuItem rules_cf = new JMenuItem("rules.cf");
 		JMenuItem ham_txt = new JMenuItem("ham.txt");
 		JMenuItem spam_txt = new JMenuItem("spam.txt");
-		
+
 		rules_cf.setFont(f2);
 		ham_txt.setFont(f2);
 		spam_txt.setFont(f2);
-		
+
 		ficheiros.add(rules_cf);
 		ficheiros.add(ham_txt);
 		ficheiros.add(spam_txt);
-		
+
 		janelaPrincipal.setJMenuBar(menuBar);
-		
-// Listener Menu
-		
+
+		// Listener Menu
+
 		rules_cf.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				FileDialog dialog = new FileDialog(janelaPrincipal,"Selecione o caminho para o ficheiro rules.cf");
-			    dialog.setMode(FileDialog.LOAD);
-			    dialog.setFile("*.txt");
-			    dialog.setVisible(true);
-			    String file = dialog.getFile();
-			    if(file!=null)  
-			          info.setText("Caminho para o ficheiro rules.cf definido");
+				FileDialog dialog = new FileDialog(janelaPrincipal, "Selecione o caminho para o ficheiro rules.cf");
+				dialog.setMode(FileDialog.LOAD);
+				dialog.setFile("*.cf");
+				dialog.setVisible(true);
+				String file = dialog.getDirectory() + dialog.getFile();
+				if (file != null) {
+					addinfo("Caminho para o ficheiro rules.cf definido");
+					controller.setRulesPath(file);
+					controller.readRules();
+					setTable();
+				}
 			}
 		});
-		
+
 		ham_txt.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				FileDialog dialog = new FileDialog(janelaPrincipal,"Selecione o caminho para o ficheiro ham.txt");
-			    dialog.setMode(FileDialog.LOAD);
-			    dialog.setFile("*.txt");
-			    dialog.setVisible(true);
-			    String file = dialog.getFile();
-			    if(file!=null)  
-			          info.setText("Caminho para o ficheiro ham.txt definido");
-			        
+				FileDialog dialog = new FileDialog(janelaPrincipal, "Selecione o caminho para o ficheiro ham.txt");
+				dialog.setMode(FileDialog.LOAD);
+				dialog.setFile("*.txt");
+				dialog.setVisible(true);
+				String file = dialog.getDirectory() + dialog.getFile();
+				if (file != null) {
+					addinfo("Caminho para o ficheiro ham.txt definido");
+					controller.setHamPath(file);
+					controller.readHam();
+				}
+
 			}
 		});
 
 		spam_txt.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				FileDialog dialog = new FileDialog(janelaPrincipal,"Selecione o caminho para o ficheiro spam.txt");
-			    dialog.setMode(FileDialog.LOAD);
-			    dialog.setFile("*.txt");
-			    dialog.setVisible(true);
-			    String file = dialog.getFile();
-			    if(file!=null)  
-			    	info.setText("Caminho para o ficheiro spam.txt definido");
+				FileDialog dialog = new FileDialog(janelaPrincipal, "Selecione o caminho para o ficheiro spam.txt");
+				dialog.setMode(FileDialog.LOAD);
+				dialog.setFile("*.txt");
+				dialog.setVisible(true);
+				String file = dialog.getDirectory() + dialog.getFile();
+				if (file != null) {
+					addinfo("Caminho para o ficheiro spam.txt definido");
+					controller.setSpamPath(file);
+					controller.readSpam();
+				}
 			}
 		});
-		
-// Info e titulo da tabela
-		
+
+		// Info e titulo da tabela
+
 		info = new JTextField();
-		info.setMinimumSize(new Dimension(400, 40));	
-		info.setFont(f);
+		info.setMinimumSize(new Dimension(400, 40));
+		info.setFont(f.deriveFont(Font.BOLD));
 		info.setForeground(Color.WHITE);
 		painel.add(info, "cell 0 0 2 1,alignx center");
 		info.setColumns(10);
 		info.setEditable(false);
 		info.setBackground(Color.BLACK);
 		info.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		JLabel regras = new JLabel("Regras e pesos respetivos");
 		painel.add(regras, "cell 0 1 2 1,alignx center");
 		regras.setFont(f);
-		
-// Botões 
-		
-		JButton random = new JButton("Aleatório");
-		random.setMinimumSize(new Dimension(140, 60));;	
-		random.setFont(f);
-		random.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-//FP e FN
-       		
+
+		// FP e FN
+
 		JLabel falsospositivos = new JLabel("Falsos Positivos");
 		painel.add(falsospositivos, "cell 2 0 1 2,alignx center");
 		falsospositivos.setFont(f);
-		
-		
-		
+
 		JLabel falsosnegativos = new JLabel("Falsos Negativos");
 		painel.add(falsosnegativos, "cell 4 0 1 2,alignx center");
 		falsosnegativos.setFont(f);
-		
+
 		fp = new JTextField();
 		painel.add(fp, "cell 2 2,alignx center,aligny center");
 		fp.setColumns(5);
 		fp.setMinimumSize(new Dimension(75, 75));
-		fp.setFont(f);
+		fp.setFont(f3);
 		fp.setEditable(false);
 		fp.setBackground(Color.WHITE);
+		fp.setHorizontalAlignment(SwingConstants.CENTER);
 		
+
 		fn = new JTextField();
 		painel.add(fn, "cell 4 2,alignx center");
 		fn.setColumns(5);
 		fn.setMinimumSize(new Dimension(75, 75));
-		fn.setFont(f);
+		fn.setFont(f3);
 		fn.setEditable(false);
 		fn.setBackground(Color.WHITE);
-		
-		painel.add(random, "cell 2 4,alignx center");
-		
-//Tabela Regras e pesos associados
-		
-		Object[][] rules = new Object[][] {
-            {"T_LOTS_OF_MONEY", 4 },
-            {"HTML_IMAGE_RATIO_04", 2},
-            {"NORMAL_HTTP_TO_IP",3},
-            {"SUBJECT_NEEDS_ENCODING",1},
-            {"LOW_PRICE",5},
-            {"T_LOTS_OF_MONEY", 4 },
-            {"HTML_IMAGE_RATIO_04", 2},
-            {"NORMAL_HTTP_TO_IP",3},
-            {"SUBJECT_NEEDS_ENCODING",1},
-            {"LOW_PRICE",5},
-            {"T_LOTS_OF_MONEY", 4 },
-            {"HTML_IMAGE_RATIO_04", 2},
-            {"NORMAL_HTTP_TO_IP",3},
-            {"SUBJECT_NEEDS_ENCODING",1},
-            {"LOW_PRICE",5},
-        };
-        String [] colunas = new String[] {"Regras" , "Peso"};
-        
-     	DefaultTableModel model = new DefaultTableModel(rules, colunas);  
-     	table= new JTable(model);
-		table.setCellSelectionEnabled(true);
-		table.setBorder(new LineBorder(new Color(0, 0, 0)));
-		table.getTableHeader().setFont(f2);
-		table.getColumnModel().getColumn(0).setPreferredWidth(500);
-		table.getTableHeader().setBackground(Color.WHITE);
-		table.setRowHeight(25);
-		table.setFont(f2);
-		DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
-		renderer.setHorizontalAlignment(SwingConstants.CENTER);
-		table.getColumnModel().getColumn(1).setCellRenderer(renderer);
+		fn.setHorizontalAlignment(SwingConstants.CENTER);
+
+		// Tabela Regras e pesos associados
+
+		colunas = new String[] { "Regras", "Peso" };
+		table = new JTable();
+		setTable();
 		scroll = new JScrollPane(table);
 		painel.add(scroll, "cell 0 2 2 5,grow");
-		
-//Botao Algoritmo
-		
+
+		// Botao Algoritmo
+
 		JButton algoritmo = new JButton("Algoritmo");
-		algoritmo.setMinimumSize(new Dimension(140, 60));;
+		algoritmo.setMinimumSize(new Dimension(140, 60));
+		;
 		algoritmo.setFont(f);
 		painel.add(algoritmo, "cell 4 4,alignx center");
 
-//Botao Iniciar
-		
+		// Botão Aleatorio
+
+		JButton random = new JButton("Aleatório");
+		random.setMinimumSize(new Dimension(140, 60));
+		;
+		random.setFont(f);
+		random.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller.pesosAleatorios();
+				setTable();
+				addinfo("Pesos aleatórios gerados");
+				botao = 1;
+			}
+		});
+
+		painel.add(random, "cell 2 4,alignx center");
+
+		// Botao Iniciar
+
 		JButton iniciar = new JButton("Iniciar");
-		iniciar.setMinimumSize(new Dimension(140, 60));;
+		iniciar.setMinimumSize(new Dimension(140, 60));
+		;
 		iniciar.setFont(f);
 		iniciar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				if (controller.ficheirosDef()) {
+					if(botao==1) {
+						controller.pesosAleatorios();
+						setTable();
+					}
+					fp.setText(String.valueOf(controller.calcularFP()));
+					fn.setText(String.valueOf(controller.calcularFN()));
+					classificar("Leisure");
+				} else {
+					addinfo("Verifique os caminhos dos ficheiros");
+				}
 			}
+
 		});
 		painel.add(iniciar, "cell 2 5 3 1,alignx center");
-		
+
 		janelaPrincipal.setVisible(true);
+
+		addinfo("Bem Vindo");
 	}
 
+	// Janela INFO
+
+	private void setTable() {
+		table.setModel(new DefaultTableModel(controller.preencherTabela(), colunas));
+		table.setCellSelectionEnabled(true);
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.getTableHeader().setFont(f2);
+		table.getColumnModel().getColumn(0).setMinWidth(300);
+		table.getTableHeader().setBackground(Color.WHITE);
+		table.setRowHeight(25);
+		table.setFont(f2);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+		renderer.setHorizontalAlignment(SwingConstants.CENTER);
+		table.getColumnModel().getColumn(1).setCellRenderer(renderer);
+		table.repaint();
+		table.revalidate();
+	}
+
+	public void addinfo(String s) {
+		new Thread(new Runnable() {
+
+			public void run() {
+				try {
+					Thread.sleep(250);
+					info.setText(s);
+					if (s.equals("Bem Vindo")) {
+						Thread.sleep(1500);
+						info.setText("Defina os caminhos para os ficheiros");
+					} else {
+						Thread.sleep(4000);
+						info.setText("");
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+	}
+
+	private void classificar(String tipo) {
+		int total = controller.calcularFP() + controller.calcularFN();
+		if (tipo.equals("Leisure")) {
+			if (controller.calcularFP() < total * 0.20) {
+				fp.setForeground(Color.GREEN);
+				fn.setForeground(Color.GREEN);
+			} else if (controller.calcularFP() < total * 0.4 && controller.calcularFP() > total * 0.2) {
+				fp.setForeground(Color.BLUE);
+				fn.setForeground(Color.BLUE);
+			} else {
+				fp.setForeground(Color.RED);
+				fn.setForeground(Color.RED);
+			}
+		} else if (tipo.equals("Professional")) {
+			if (controller.calcularFP() > total * 0.80) {
+				fp.setForeground(Color.GREEN);
+				fn.setForeground(Color.GREEN);
+			} else if (controller.calcularFP() > total * 0.6 && controller.calcularFP() < total * 0.8) {
+				fp.setForeground(Color.BLUE);
+				fn.setForeground(Color.BLUE);
+			} else {
+				fp.setForeground(Color.RED);
+				fn.setForeground(Color.RED);
+			}
+		} else if (tipo.equals("Leisure and Professional")) {
+			if (controller.calcularFP() > total * 0.45 && controller.calcularFP() < total * 0.55) {
+				fp.setForeground(Color.GREEN);
+				fn.setForeground(Color.GREEN);
+			} else if (controller.calcularFP() < total * 0.45 || controller.calcularFP() > total * 0.55) {
+				fp.setForeground(Color.BLUE);
+			}
+		}
+
+	}
 }
